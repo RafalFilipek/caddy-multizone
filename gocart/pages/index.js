7196c28680886df2bdc53b50a275d3887c7f794d
Rafau <rafal.filipek@gmail.com>
2022-09-22 15:32:09 +0200

    Add files via upload

---
+++ gocart/pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div>
+      <h1>Gocart</h1>
+      <hr />
+      <Link href="/">
+        <a>go to portal</a>
+      </Link>
+    </div>
+  );
+}
---
+++ portal/pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div>
+      <h1>Portal</h1>
+      <hr />
+      <Link href="gocart">
+        <a>go to gocart</a>
+      </Link>
+    </div>
+  );
+}
